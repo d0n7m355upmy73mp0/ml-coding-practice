3c43a00df50c04b02f15457005bc2914e6ce65d8
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-10 00:45:17 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -86,3 +86,12 @@ print(b)
 print(b.split(','))
 
 # 리스트 만들기
+a = [1, 2, 3]
+b = ['Life', 'is', 'too', 'short']
+c = [1, 2, 'Life', 'Is']
+d = [1, 2, [3, 4], ['Life', 'is']]
+
+# 리스트 인덱싱
+print(d[0])
+print(d[2])
+print(d[3][-1])
