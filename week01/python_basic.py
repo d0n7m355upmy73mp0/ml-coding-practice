395d0dfd317154c26e82758a9c36a842543c5c2e
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-10 00:38:17 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -71,8 +71,8 @@ print(d.strip())
 
 # 문자열 수정 (불가능)
 a = "Pithon"
-a[1] = 'y' # 오류 발생
+# a[1] = 'y' # 오류 발생
 
 a = "Python is difficult."
-print(a.replace("difficult", "funny"))
+print(a.replace("diffi", "funny"))
 print(a)
