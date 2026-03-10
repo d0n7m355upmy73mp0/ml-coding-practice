7839587d3df8d0b22139306d15ae99cd2014e0e2
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-10 16:51:02 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -229,5 +229,15 @@ print(s3)
 print(s2 & s3)
 print(s2.intersection(s3))
 
+# 합집합 연산
+print(s2 | s3)
+print(s2.union(s3))
+
+# 차집합 연산
+print(s2 - s3)
+print(s3 - s2)
+print(s2.difference(s3))
+print(s3.difference(s2))
+
 
 
