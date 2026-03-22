21ee8c210ccbf8ba88921050fb5ea02a2071f0bc
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-22 12:07:03 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -71,5 +71,12 @@ data = {
     'is_spam': [False, False, True, False, False, True, False, False],
     'has_image': [True, False, True, True, False, False, True, True]
 }
+df = pd.DataFrame(data)
+print(df.head())
+
+# 필터링 조건 설정
+condition = (
+    df['comment_length']
+)
 
 
