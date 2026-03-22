8b7812b24cd4ff9dcb59de0d00a1339e2477d53a
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-22 12:44:38 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -109,6 +109,6 @@ print(iris.drop('class', axis=1).agg(['sum', 'mean', 'std']))
 # print(iris.agg(['sum, 'mean' 'std']))     # class 컬럼으로 인해 에러 발생
 
 
-print(iris['class'.unique()])
+print(iris['class'].unique())
 print(iris['class'].nunique())
 print(iris['class'].value_counts())
