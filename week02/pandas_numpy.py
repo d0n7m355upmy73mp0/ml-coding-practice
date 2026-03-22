2d49597a6de170d13b1a5dba142aae5a4b297a15
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-22 14:46:54 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -142,4 +142,11 @@ print(df.head())
 
 # 개인별 과목 점수의 평균값 계산 (axis=1)
 df['average'] = df[['math', 'science', 'english']].mean(axis=1)
+print(df)
+
+# 이름과 평균값만을 포함하는 새로운 데이터프레임 생성
+average_df = df[['name', 'average']]
 print(average_df)
+
+# 2.2 넘파이
+import numpy as np
