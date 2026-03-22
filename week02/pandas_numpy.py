f325ad4680357ecbc033faa3c5fd45d15a16cb39
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-22 12:08:15 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -76,7 +76,10 @@ print(df.head())
 
 # 필터링 조건 설정
 condition = (
-    (df['comment_length']
+    (df['comment_length'] >= 100) &
+    (df['likes'] >= 20) &
+    (~df['is_spam']) &
+    (df['has_image'])
 )
 
 
