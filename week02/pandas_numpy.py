62a999466988659289860c0c5fd147cc35c69238
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-22 12:46:38 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -97,7 +97,7 @@ print(sample_df.describe())
 print(sample_df.std())
 print(sample_df.agg(['count', 'mean', 'std', 'min', 'max']))
 
-flie_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_soutce/main/iris.cvs'
+flie_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/iris.csv'
 iris = pd.read_csv(file_url)
 
 print(iris.head())
