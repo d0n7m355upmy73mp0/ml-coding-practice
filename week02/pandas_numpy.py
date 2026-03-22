6395ccbad35d3c59a69fddd98d71dbb7642659d9
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-22 14:21:40 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -102,16 +102,16 @@ iris = pd.read_csv(file_url)
 
 print(iris.head())
 
-print(iris.groupby('class').std())
+# print(iris.groupby('class').std())
 
-print(iris.drop('class', axis=1).agg(['sum', 'mean', 'std']))
+# print(iris.drop('class', axis=1).agg(['sum', 'mean', 'std']))
 
-# print(iris.agg(['sum, 'mean' 'std']))     # class 컬럼으로 인해 에러 발생
+# # print(iris.agg(['sum, 'mean' 'std']))     # class 컬럼으로 인해 에러 발생
 
 
-print(iris['class'].unique())
-print(iris['class'].nunique())
-print(iris['class'].value_counts())
+# print(iris['class'].unique())
+# print(iris['class'].nunique())
+# print(iris['class'].value_counts())
 
 # 예제 데이터 생성
 data = {
