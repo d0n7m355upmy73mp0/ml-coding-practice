83e01a94bc01404ce6cf2443528a766620fab203
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-26 08:47:51 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -91,3 +91,9 @@ print(titanic.info())
 plt.figure(figsize=(12, 8))
 scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic['Surevived'],cmap='Set2', alpha=0.7)
 
+plt.title('Age and Fare Relationship with Survival on the Titanic')
+plt.xlabel('Age')
+plt.ylabel('Fare')
+plt.legend(handles=scatter.legend_elements()[0], title='Survived',
+           lavels=['Not Survived', 'Survived'], loc='upper right')
+plt.savefig('F')
