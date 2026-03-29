04b16046b064e53725baf7cf275303db6adaf1a3
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-29 11:00:30 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -179,4 +179,10 @@ plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
                  color='hotpink', alpha=0.6, label='Not Survived')
 
 plt.title('Survival by Age Group on Titanic')
-plt.xlabe('Age')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.xtricks(rotation=45)
+plt.legend()
+plt.grid(True)
+plt.savefig('Figure08.png')
+plt.close()
