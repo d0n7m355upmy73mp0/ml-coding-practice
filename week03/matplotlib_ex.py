eb4e442a89747698a952c3421877986c5fd9d17a
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-03-29 15:41:29 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -298,7 +298,7 @@ plt.plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7,
 label='Not Survived')
 plt.xlabel('Parch')
 plt.ylabel('Not Survived Count', color='indigo')
-plt.rick_params(axis='y', lavelcolor='indigo')
+plt.tick_params(axis='y', lavelcolor='indigo')
 plt.legend(loc='upper right')
 
 # 두 번째 서브플롯 설정(막대 그래프)
