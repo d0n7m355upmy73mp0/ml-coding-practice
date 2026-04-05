44405ea85f03653f44c43ccedcf3e043761d68b7
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-05 14:25:11 +0900

    뭐지 이거

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # 데이터 준비
-from pathlib import Path
 import numpy as np
 import pandas as pd
-import tarfile
-import urllib.request
 
-def load_housing_data():
-    tarball_path = Path("datasets")
+housing = pd.read_csv('./week04/housing.csv')       # 오류 발생 시, ./housing.csv 파일로도 시도
+
+# 테스트 세트 만들기
+from sklearn.model_selection import train_test_split
+
+housing["income_cat"] = pd.cut(housing["median+income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
