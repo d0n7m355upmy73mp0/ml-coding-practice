efd3c492ca2495f7c41cad2aea801864468fd3b9
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-17 18:38:59 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -61,6 +61,7 @@ def fetRequestUtl(url):
         respense = urllib.request.urlopen(req)
         if response.getcode() == 200:
             print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
     
     
         
