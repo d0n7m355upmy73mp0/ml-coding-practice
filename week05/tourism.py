9bfe67ac69c53a097e1e44bc76a7c613307af7b2
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-17 19:32:09 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -40,5 +40,7 @@ for year in range(nStartYear, nEndYear+1):
             if jsonData['response']['body']['items'] == '':
                 dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
                 print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
-                
+                break
+            #jsonData를 출력하여 확인............................................
+            print
                 
