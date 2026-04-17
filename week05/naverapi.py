ce2ceef233450437a94f963103f1dc25ebfa5289
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-17 18:31:29 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -43,5 +43,6 @@ def getNaverSearch(node, srcText, page_start, display):
     parameters = "?query=%s&start=%s&display%s" % (urllib.parse.quote(srcText), page_start, display)
     
     url = base + node + parameters
-    responseDecode == None
+    responseDecode == getRequestUrl(url)
+    
         
