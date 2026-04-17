c93da59707f41ed11e8ea4cc866f1646ec7aa31c
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-17 18:59:30 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -83,7 +83,10 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
     jsonResult.append({
         'cnt': cnt,
         'title': title,
-        ''
+        'description' : description,
+        'org_link': org_link,
+        'link': link,
+        'pDate': pData
     })
     
         
