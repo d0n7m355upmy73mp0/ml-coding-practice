a5c7710ed3f392c61fa2b367bae8f23cd002caa1
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-20 23:15:37 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -67,3 +67,9 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     url = service_url + parameters
     
     responseDecode = getRequestUrl(url)                       #[CODE 1]
+    
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+    
