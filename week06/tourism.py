8ab3402769498acedd75d7376c1dbac9328cb64a
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-20 22:44:08 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -46,6 +46,6 @@ def main():
                 
                 natName = jsonData['response']['body']['items']['item']['natKorNm']
                 natName = natName.replace(' ', '')
-                
+                num = jsonData['response']['body']['item'][]
                                                                                                         
         
