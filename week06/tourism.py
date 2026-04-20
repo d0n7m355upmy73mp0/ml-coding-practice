ce63b36b7aaec56c93328a3c393264aad494694e
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-20 23:27:07 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -84,7 +84,7 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
             return response.read().decode('utf-8')
         except Exception as e:
             print(e)
-            print("[%s] Url Request Success" % datetime.datetime.nov()))
+            print("[%s] Url Request Success" % datetime.datetime.now()))
             return None
     
 main()
