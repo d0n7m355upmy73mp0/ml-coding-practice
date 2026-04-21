6e2a9a4eb8f8c20c80bf948e791c6efcce0817dc
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-21 18:36:59 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -4,4 +4,14 @@ import datetime
 import json
 
 client_id = '4I_PCGeCfT46gzmmiHtm'
-client_secret = ''
+client_secret = '4Li19IrMMi'
+
+def main():
+    
+    node = 'news'
+    srcText = input('검색어를 입력하세요: ')
+    
+    cnt = 0
+    jsonResponse = []
+    
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)    #
