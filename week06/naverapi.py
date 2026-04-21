5e4b1b40328707d00aedeef5e44c78cf9fb1ae48
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-21 18:37:59 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -8,10 +8,14 @@ client_secret = '4Li19IrMMi'
 
 def main():
     
-    node = 'news'
+    node = 'news'                                           # 크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
     
     cnt = 0
     jsonResponse = []
     
-    jsonResponse = getNaverSearch(node, srcText, 1, 100)    #
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)    # [CODE 2]
+    total = jsonResponse['total']
+    
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
+        for post in jsonResponse['items']:
