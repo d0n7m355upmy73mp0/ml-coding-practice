c8f5370a186272ea461bd54a3d8b2db9b25ecb5c
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-04-21 18:39:59 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -19,3 +19,12 @@ def main():
     
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
+            cnt += 1
+            getPostData(post, jsonResult, cnt)              # [CODE 3]
+            
+        start = jsonResponse['start'] + jsonResponse['display']
+        jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
+        
+    print('전체 검색 : %d 건' %total)
+    
+    with open('%s_')
