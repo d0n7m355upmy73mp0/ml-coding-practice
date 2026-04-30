7ea4490628832cdd48d189cecc601568bf8f2b10
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-05-01 00:26:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 from sklearn.datasets import fetch_openml
+
+mnist = fetch_openml('mnist_784', as_frame=False)
+
+print(mnist.keys())   # data와 target만 사용
+
+x, y = mnist.data, mnist.from django.utils.translation import ugettext_lazy as _
