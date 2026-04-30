95763e792f17307c64edd038b56191b7d487279e
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-05-01 00:52:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -9,7 +9,7 @@ X, y = mnist.data, mnist.target
 print(X)
 print(X.shape)      # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
 print(y)
-print(y,shape)
+print(y.shape)
 
 import matplotlib.pyplot as plt
 
@@ -18,7 +18,7 @@ def plot_digit(image_data):
     plt.imshow(image, cmap="binary")
     plt.axis("off")
     
-some_difit = X[0]
+some_digit = X[0]
 plot_digit(some_digit)
 plt.show()
 
