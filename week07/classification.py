a0235eab5258c6ca5039549a020b27daf615b384
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-05-01 00:27:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -5,4 +5,9 @@ mnist = fetch_openml('mnist_784', as_frame=False)
 
 print(mnist.keys())   # data와 target만 사용
 
-x, y = mnist.data, mnist.from django.utils.translation import ugettext_lazy as _
+x, y = mnist.data, mnist.target
+print(x)
+print(x.shape)      # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y,shape)
+
