04884bbdb674bc6caa10546138314ee52db95406
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-05-01 00:49:11 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -91,4 +91,12 @@ y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
 roc_auc_score(y_train_5, y_scores)
 
 #다음은 실행하는데 몇 분이 걸릴 수 있음
-from sklearn.enseble import RandomFores
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+
+y_provas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    method="predict_proba")
+
+y_scores_forest = y_probas_forest[:, 1]
+y_train_pred_forest = y_probas_forest
