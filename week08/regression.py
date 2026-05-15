d4f59106cd0ca3c1ba3c3078d09115a5570e7bc0
d0n7m355upmy73mp0 <rosencos17@gmail.com>
2026-05-15 21:26:07 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -14,4 +14,18 @@ m = 100                                 # 샘플 개수
 X = 2 * np.random.rand(m, 1)            # 열 벡터
 y = 4 * 3 * X + np.random.randn(m, 1)   # 열 벡터
 
-import matplotlb.pyplot as plt
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$", rotation=0)
+plt.axis([0, 2, 0, 15])
+plt.grid()
+plt.show()
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
